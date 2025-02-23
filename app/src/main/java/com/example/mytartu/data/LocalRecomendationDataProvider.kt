ddf690a046873c357b21162aeac1cae802886dea
Aren <nerelorenzo18@gmail.com>
2025-02-23 11:15:15 +0100

    arreglé errores y eso

---
+++ app/src/main/java/com/example/mytartu/data/LocalRecomendationDataProvider.kt
@@ -0,0 +1,59 @@
+package com.example.mytartu.data
+
+import com.example.mytartu.R
+import com.example.mytartu.model.RecomendationItem
+
+object LocalRecomendationDataProvider {
+
+    fun getHotels() : List<RecomendationItem>{
+        return listOf(
+            RecomendationItem(
+                id = 1,
+                imageResourceId = R.drawable.la_bochi,
+                name = R.string.hotels,
+                description = R.string.hotels,
+                details = R.string.hotels
+            ),
+            RecomendationItem(
+                id = 1,
+                imageResourceId = R.drawable.la_bochi,
+                name = R.string.hotels,
+                description = R.string.hotels,
+                details = R.string.hotels
+            )
+        )
+    }
+    fun getRestaurants() : List<RecomendationItem>{
+        return listOf(
+            RecomendationItem(
+                id = 2,
+                imageResourceId = R.drawable.la_bochi,
+                name = R.string.restaurants,
+                description = R.string.restaurants,
+                details = R.string.restaurants
+            )
+        )
+    }
+    fun getParks() : List<RecomendationItem>{
+        return listOf(
+            RecomendationItem(
+                id = 3,
+                imageResourceId = R.drawable.la_bochi,
+                name = R.string.parks,
+                description = R.string.parks,
+                details = R.string.parks
+            )
+        )
+    }
+    fun getShoppingMalls() : List<RecomendationItem>{
+        return listOf(
+            RecomendationItem(
+                id = 4,
+                imageResourceId = R.drawable.la_bochi,
+                name = R.string.malls,
+                description = R.string.malls,
+                details = R.string.malls
+            )
+        )
+    }
+}
--- app/src/main/java/com/example/mytartu/ui/BaseMenuScreen.kt
+++ app/src/main/java/com/example/mytartu/ui/BaseMenuScreen.kt
@@ -4,17 +4,26 @@ package com.example.mytartu.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mytartu.R
+import com.example.mytartu.data.LocalRecomendationDataProvider
 import com.example.mytartu.model.RecomendationItem
+import com.example.mytartu.ui.theme.MyTartuTheme
 
 //TODO: esto es la apariencia basica de los menus de las apps
 @Composable
 fun BaseMenuScreen(
-
+    options: List<RecomendationItem>
 ){
 
 
@@ -27,13 +36,19 @@ fun MenuItem(
     modifier: Modifier = Modifier
 ){
     Card(
-        onClick = {onItemClick(RecomendationItem)},
+        onClick = {onItemClick(recomendation)},
         modifier = modifier
     ){
-        Row(){
-            Image()
+        Row(
+            modifier = modifier.fillMaxWidth()
+        ){
+            Image(
+                painter = painterResource(recomendation.imageResourceId),
+                contentDescription = null
+            )
             Column {
-                Text()
+                Text(stringResource(recomendation.name))
+                Text(stringResource(recomendation.details))
             }
         }
     }
@@ -45,12 +60,37 @@ fun MenuItemList(
     onClick: (RecomendationItem) -> Unit,
     modifier: Modifier = Modifier
 ){
-    LazyRow {
-        items(recomendations, key = { recomendationItem -> recomendationItem.id }) { recomendationItem ->
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(recomendations, key = { recomendation -> recomendation.id }) { recomendation ->
             MenuItem(
-                recomendation = recomendationItem,
+                recomendation = recomendation,
                 onItemClick = onClick
             )
         }
+    }
+}
+
+//@Preview
+//@Composable
+//fun MenuItemPreview(){
+//    MyTartuTheme {
+//        MenuItem(
+//            recomendation = LocalRecomendationDataProvider.getHotels()[0],
+//            onItemClick = {}
+//        )
+//    }
+//
+//}
+
+@Preview
+@Composable
+fun MenuListPreview(){
+    MyTartuTheme{
+        MenuItemList(
+            recomendations = LocalRecomendationDataProvider.getHotels(),
+            onClick = {}
+        )
     }
 }
